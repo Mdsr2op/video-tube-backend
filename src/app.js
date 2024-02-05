537d05e2631d6e7d453e45bf98f7f6e855c0ecbe
Mdsr2op <mdsrmfkhan@gmail.com>
2024-02-05 12:05:04 +0500

    Added routes and controllers for healthcheck

--- src/app.js
+++ src/app.js
@@ -19,11 +19,13 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 //routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 
 export { app }
---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,15 @@
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const healthcheck = asyncHandler(async (req, res) => {
+     return res
+     .status(200)
+     .json( new ApiResponse(200, {}, "Everything is ok!"))
+})
+
+export {
+    healthcheck
+    }
+    
