fe8147cf40e1893ca899a70648a556fb2d7c3bfa
Mdsr2op <mdsrmfkhan@gmail.com>
2026-05-07 14:56:06 +0500

    updated health route

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -6,7 +6,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const healthcheck = asyncHandler(async (req, res) => {
      return res
      .status(200)
-     .json( new ApiResponse(200, {}, "Everything is ok!"))
+     .json( new ApiResponse(200, {}, "Everything is ALRIGHT!"))
 })
 
 export {
